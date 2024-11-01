8f4b9da53963f99b16d2a0bb939afd6112cfffe8
Harsh sharma <harsh@Harshs-MacBook-Air.local>
2024-11-01 23:32:52 +0530

    first commit

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,12 +1,11 @@
 import express from 'express';
 import { login, logout, register, getUserData } from '../controller/authController.js';
-import authenticate from '../middleware/authenticate.js';
 
 const router = express.Router();
 
 router.post('/login', login);
 router.post('/logout', logout);
-router.post('/register', register);
-router.get('/user', authenticate, getUserData); 
+router.post('/register', register); 
+router.get('/user', getUserData);  
 
 export default router;
